2d002d28e9d745000686e4e5f0a3df4e23621cec
KisukeMaybe <musab.sudirman@gmail.com>
2025-07-04 11:50:53 +0800

    Implemented hero section

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { navLinks } from '../../constants'
-import useGSAP from '@gsap/react'
+import { useGSAP } from '@gsap/react'
 import gsap from 'gsap'
 
 const Navbar = () => {
